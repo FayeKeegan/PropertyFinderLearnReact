ba630366c5f072027ba5a5666e09755e800cbcaa
Faye Keegan <faye.keegan@gmail.com>
2018-06-05 10:57:56 -0700

    get rid of redux etc, add blank modal component

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,7 @@ import {
   ActivityIndicator
 } from 'react-native';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
-import * as Actions from '../actions'; //Import your actions
-
-class Home extends Component {
+export default class Home extends Component {
   constructor(props) {
     super(props);
 
@@ -61,26 +56,6 @@ class Home extends Component {
     );
   }
 }
-
-// The function takes data from the app current state,
-// and insert/links it into the props of our component.
-// This function makes Redux know that this component needs to be passed a piece of the state
-function mapStateToProps(state, props) {
-  return {
-    loading: state.dataReducer.loading,
-    data: state.dataReducer.data
-  };
-}
-
-// Doing this merges our actions into the component’s props,
-// while wrapping them in dispatch() so that they immediately dispatch an Action.
-// Just by doing this, we will have access to the actions defined in out actions file (action/home.js)
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Actions, dispatch);
-}
-
-//Connect everything
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 const styles = StyleSheet.create({
   activityIndicatorContainer: {
